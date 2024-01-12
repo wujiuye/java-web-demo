81c567155b6c734d9015965db8f5d831adb63612
wujiuye <wujiuye@lizhi.fm>
2024-01-12 17:37:14 +0800

    add build config

--- src/main/java/fm/lizhi/common/nebula/webdemo/controller/DemoController.java
+++ src/main/java/fm/lizhi/common/nebula/webdemo/controller/DemoController.java
@@ -1,7 +1,6 @@
 package fm.lizhi.common.nebula.webdemo.controller;
 
 import com.wujiuye.jsonparser.core.JsonUtils;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
